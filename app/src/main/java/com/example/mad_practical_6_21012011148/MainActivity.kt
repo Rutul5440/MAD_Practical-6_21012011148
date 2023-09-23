6a57026aed46a4b6993c15e7a165222d8bf06219
rutul <rutulsuthar2018@gmail.com>
2023-09-23 16:20:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_6_21012011148/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.mad_practical_6_21012011148
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val playButton:FloatingActionButton = findViewById(R.id.play)
+        playButton.setOnClickListener {
+            PlayPause()
+        }
+
+        val pauseButton:FloatingActionButton = findViewById(R.id.pause)
+        pauseButton.setOnClickListener {
+            Stop()
+        }
+    }
+    fun PlayPause(){
+        Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,MyService.PLAYERVALUE).apply {
+            startService(this)
+        }
+    }
+
+    fun Stop(){
+        Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,MyService.PLAYERVALUE).apply {
+            stopService(this)
+        }
+    }
+}
